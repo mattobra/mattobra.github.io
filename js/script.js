25cb3aaabc729cf761849dde77ac304454c48a8f
asaki444 <asaki444@gmail.com>
2016-10-08 16:03:32 -0400

    added page.html file to the authorization url

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-var LinkedinAuth = "https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=75rsv9vpa1ixv3&redirect_uri=https%3A%2F%2Fwww.aditidevelops.com&state=987654321&scope=r_basicprofile"
+var LinkedinAuth = "https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=75rsv9vpa1ixv3&redirect_uri=https%3A%2F%2Fmattobra.github.io/page.html&state=987654321&scope=r_basicprofile"
 var start = function(){ $.getJSON(LinkedinAuth).then(function(data){
   console.log(data);
 })}
