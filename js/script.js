60c870226529e7638234a015bc0e8f6526d8b984
asaki444 <asaki444@gmail.com>
2016-10-09 11:32:04 -0400

    comment out old code

--- js/script.js
+++ js/script.js
@@ -8,15 +8,5 @@ $("#start").click(
 function(){
 location.replace(LinkedinAuth)
 }
-)*/
-var skill1;
-var skill2;
-$("#anchor #skills").click(
-function(){
-skill1 = $("#skill1").val().toString();
-skill2= $("#skill2").val();
- console.log(skill1,skill2);}
-);
-$('textarea').html("Hi "+ skill1 + " ,My name is YOUR NAME HERE and I’m currently a (position) at (company). We met at "+skill2+" this event")
-console.log(skill1,skill2);
+*/
 })
