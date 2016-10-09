d3e0dfe5bcea2819b3d5a3094a24e498e8c94aee
asaki444 <asaki444@gmail.com>
2016-10-09 11:07:29 -0400

    comment out old code

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 var LinkedinAuth = "https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=75rsv9vpa1ixv3&redirect_uri=https%3A%2F%2Fmattobra.github.io/page.html&state=987654321&scope=r_basicprofile"
-var start = function(){ $.getJSON(LinkedinAuth).then(function(data){
+/*var start = function(){ $.getJSON(LinkedinAuth).then(function(data){
   console.log(data);
 })}
 console.log("ready")
@@ -8,7 +8,7 @@ $("#start").click(
 function(){
 location.replace(LinkedinAuth)
 }
-)
+)*/
 var skill1;
 var skill2;
 $("#anchor #skills").click(
