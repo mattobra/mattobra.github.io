f2665a00473555a4ff4b23359662b17ad453e45a
Maxine Attobrah <mattobra@users.noreply.github.com>
2016-10-09 07:52:22 -0400

    Testing displaying skill1 and skills 2 on template page

--- js/script.js
+++ js/script.js
@@ -18,4 +18,5 @@ skill2= $("#skill2").val();
  console.log(skill1,skill2);}
 );
 $('textarea').html("Hi "+ skill1 + " ,My name is YOUR NAME HERE and I’m currently a (position) at (company). We met at "+skill2+" this event")
+console.log(skill1,skill2);
 })
